925b488f35ef6fdc583c9b1adba208a27d9e11ea
Marcos Magueta <maguetamarcos@gmail.com>
2024-12-19 02:36:08 -0300

    decisioner

--- event.el
+++ event.el
@@ -1,8 +1,43 @@
 
 (defclass event ()
-  ((abc :initarg :abc
-	:accessor :abc)))
+  ((name :initarg :name
+	 :accessor :name)
+   (content :initarg :content
+	    :accessor :content)))
 
-(:abc (make-instance 'event :abc 1))
+(defclass state ()
+  ((name :initarg :name
+	 :accessor :name)
+   (content :initarg :content
+	    :accessor :content)))
+
+(defun event-server (stream-id)
+  (make-instance 'state :name nil :content nil))
+
+(defun event-committer (stream-id decision-name result)
+  (pcase result
+    (`(!failure ,msg) (error msg))
+    (`(!success ,events) (print (format "[DECIDED][%s][%s]: %s" decision-name stream-id events)))))
+
+(defmacro defdecision (decision-name stream-id-command &rest body)
+  (declare (indent defun))
+  `(let ((stream-id ,(car stream-id-command))
+	 (state ,(event-server (car stream-id-command)))
+	 (command ,(cadr stream-id-command)))
+     (event-committer stream-id ',decision-name ,@body)))
+
+(defmacro defstruct (&rest args)
+  `(cl-defstruct ,@args))
+
+(defstruct issue-order order-id annotation)
+
+(defdecision attempt-to-issue-order ("billing-00001" (make-issue-order :order-id 217313 :annotation "pizza margherita"))
+  (progn
+    (print (:name state))
+    (print (issue-order-annotation command))
+    `(!success ((make-instance 'event
+			       :name 'order-issued
+			       :content '((:order-id . ,(issue-order-order-id command))
+					  (:annotation . ,(issue-order-annotation command))))))))
 
 (provide 'event)
